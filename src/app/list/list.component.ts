5118aa1e8b850021e2091cec700f53781f457def
Jennifer Wadella <jennifer.wadella@gmail.com>
2021-06-06 21:01:37 -0500

    reactive code

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -2,9 +2,10 @@ import { Component, OnDestroy, OnInit, QueryList, ViewChild, ViewChildren } from
 import { Villager, Personality, Species, Hobby, VillagerSortOptions, ApiService} from '../api.service';
 import { MatCheckbox, MatCheckboxChange } from '@angular/material/checkbox';
 import { MatExpansionPanel} from '@angular/material/expansion';
-import { MatSelectChange } from '@angular/material/select';
-import { FormControl } from '@angular/forms';import { Observable, Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import { FormControl } from '@angular/forms';
+import { BehaviorSubject, combineLatest, fromEvent, merge, Observable, Subject } from 'rxjs';
+import { map, mapTo, startWith, shareReplay, tap } from 'rxjs/operators';
+import { MatButton } from '@angular/material/button';
 
 interface CheckboxGroup {
   attribute: VillagerSortOptions,
@@ -19,9 +20,9 @@ export class ListComponent implements OnInit, OnDestroy {
   private unsubscriber$ = new Subject<void>();
   @ViewChild(MatExpansionPanel) accordion!: MatExpansionPanel;
   @ViewChildren(MatCheckbox) checkboxes!: QueryList<MatCheckbox>;
+  @ViewChild('resetButton', { static: true }) button!: MatButton;
 
-  private villagers: Villager[] = [];
-  public displayedVillagers: Villager[] = [];
+  public displayedVillagers$!: Observable<Villager[]>;
   private checkSelection: VillagerSortOptions[] = [];
   public sortOptions = ["personality", "species", "hobby", "birthday", "name"];
   public checkBoxList: CheckboxGroup[] = [
@@ -38,21 +39,54 @@ export class ListComponent implements OnInit, OnDestroy {
       list: Object.values(Hobby)
     }
   ]
-  public sortOption = new FormControl({value: '', disabled: false});
+  public sortOptionControl = new FormControl({value: null, disabled: false});
+  private filterOptions$ = new BehaviorSubject<VillagerSortOptions[]>([]);
+  public displayedVillagerCount$!: Observable<number>;
 
   constructor(private apiService: ApiService) { }
 
   ngOnInit(): void {
-    this.apiService.getVillagers().subscribe((villagers: Villager[]) => {
-      this.displayedVillagers = this.villagers = this.sortList(villagers, "name");
-    });
+    const villagers$ = this.apiService.getVillagers().pipe(
+      map((villagers) => {
+        // we're just pretending this is our initial sort
+        return this.sortList(villagers, "name")
+      }),
+      shareReplay()
+    );
+    
+    const resetEvent$ = fromEvent(this.button._elementRef.nativeElement, 'click').pipe(
+      tap(() => {
+        this.checkSelection = [];
+        this.checkboxes.forEach((checkbox) => {
+          if(checkbox.checked) {
+            checkbox.toggle();
+          }
+        })
+      })
+    );
 
-    this.sortOption.valueChanges.pipe(
-      takeUntil(this.unsubscriber$)
+    const sortReset$ = resetEvent$.pipe(
+      mapTo(null)
+    );
+    const sortOption$ = merge(this.sortOptionControl.valueChanges.pipe(startWith(null)), sortReset$);
+
+    const filterReset$ = resetEvent$.pipe(
+      mapTo([])
     )
-    .subscribe((value) => {
-      this.setSort(value);
-    })
+    const filterOptions$ = merge(this.filterOptions$, filterReset$)
+
+    this.displayedVillagers$ = combineLatest([villagers$, sortOption$, filterOptions$]).pipe(
+      map(([villagers, sortOption, filterOption]) => {
+          const sortedList =  sortOption ? this.sortList(villagers, sortOption) : villagers;
+          return filterOption.length ? this.filterList(sortedList, filterOption): sortedList
+        })
+    );
+
+    this.displayedVillagerCount$ = this.displayedVillagers$.pipe(
+      map((villagers) => {
+        return villagers.length
+      })
+    )
   }
 
   ngOnDestroy(): void {
@@ -61,8 +95,9 @@ export class ListComponent implements OnInit, OnDestroy {
   }
 
   sortList(list: Villager[], property: VillagerSortOptions): Villager[] {
+    const sortedList = [...list];
     if(property === "birthday") {
-      return list.sort(function(a,b){
+      return sortedList.sort(function(a,b){
         var [dayA, monthA] = a.birthday.split('/');
         var dateA = new Date(2020, parseInt(monthA, 10), parseInt(dayA, 10));
 
@@ -73,7 +108,7 @@ export class ListComponent implements OnInit, OnDestroy {
       });
     }
     else if(property === "name") {
-      return list.sort(function(a, b) {
+      return sortedList.sort(function(a, b) {
         var nameA = a.name['name-USen'].toUpperCase();
         var nameB = b.name['name-USen'].toUpperCase();
   
@@ -87,7 +122,7 @@ export class ListComponent implements OnInit, OnDestroy {
       });
     }
     else {
-      return list.sort(function(a, b) {
+      return sortedList.sort(function(a, b) {
         var nameA = a[property].toUpperCase();
         var nameB = b[property].toUpperCase();
   
@@ -103,53 +138,22 @@ export class ListComponent implements OnInit, OnDestroy {
 
   }
 
-  filterList() {
-    this.accordion.close();
-    const filters = this.checkSelection;
-    this.displayedVillagers = this.villagers.filter((villager: Villager) => {
+  filterList(list: Villager[], filters: VillagerSortOptions[]): Villager[] {
+    return list.filter((villager: Villager) => {
       const villagerVals = Object.values(villager);
       const villagerHasTrait = villagerVals.some(r=> filters.includes(r));
       return villagerHasTrait;
     })
   }
 
-  showFavorites(change: any) {
+  applyFilters() {
     this.accordion.close();
-    if(change.checked) {
-      this.displayedVillagers = this.villagers.filter(villager => villager.favorite);
-    }
-    else {
-      this.filterList();
-    }
-  }
-
-  setSort(value: VillagerSortOptions): void {
-    this.displayedVillagers = this.sortList(this.villagers, value);
+    this.filterOptions$.next(this.checkSelection);
   }
 
   checkboxChecked(change: MatCheckboxChange) {
     const checkedValue = change.source.value as VillagerSortOptions;
     this.checkSelection  = change.checked ? [...this.checkSelection, checkedValue] : this.checkSelection.filter(item => item !== checkedValue)
-  }
-
-  favoriteVillager(event: any, villagerToFav: Villager) {
-    event.stopPropagation();
-    this.villagers = this.villagers.map((villager) => {
-      if(villager.id === villagerToFav.id) {
-        villager.favorite = !villager.favorite;
-      }
-      return villager;
-    });
-  }
-
-  reset() {
-    this.displayedVillagers = this.villagers;
-    this.checkSelection = [];
-    this.checkboxes.forEach((checkbox) => {
-      if(checkbox.checked) {
-        checkbox.toggle();
-      }
-    })
   }
 
 }
